3d88804b0a1d70fc820d2f2e4110bf73fd076082
Michel Caiafa <michel.caiafa@gmail.com>
2024-03-06 22:08:08 -0300

    feat: Adicionando logs.

--- src/bronze.py
+++ src/bronze.py
@@ -1,18 +1,17 @@
-import wget, os, time
-import concurrent.futures
-from functools import partial
+import wget, os
 from zipfile import ZipFile
 import pyarrow.csv as pv
 import pyarrow.parquet as pq
-
+import logs_decorator
 
 URL = 'https://dadosabertos.rfb.gov.br/CNPJ/'
 EMPRESAS = 'Empresas1.zip'
 SOCIOS =    'Socios0.zip'
 FILES = [EMPRESAS,SOCIOS]
-BRONZE = '/home/caiafa/Stone/BRONZE/'
-INGESTION = '/home/caiafa/Stone/BRONZE/INGESTION/'
-#ETAPA 1
+BRONZE = '/home/caiafa/pipeline_empresas_brasil/BRONZE/'
+INGESTION = '/home/caiafa/pipeline_empresas_brasil/BRONZE/INGESTION/'
+
+@logs_decorator.tempo_execucao
 def downlod_file(filename: str, url: str, output: str) -> bool:
 
     try:
@@ -22,7 +21,8 @@ def downlod_file(filename: str, url: str, output: str) -> bool:
     except Exception as error:
         print(f"Ocorreu o erro {error}")
         return False
-#etapa 2
+    
+#Etapa 2
 def unzip_file(filename: str, path: str, output: str) -> bool:
     try:
         with ZipFile(path+filename, 'r') as zObject:
@@ -32,30 +32,20 @@ def unzip_file(filename: str, path: str, output: str) -> bool:
         print(f'Ocorreu um erro {error}')
         return False
 
-
-downlod_multi_files = partial(downlod_file,url = URL, output = INGESTION)
-with concurrent.futures.ThreadPoolExecutor() as executor:
-     files = executor.map(downlod_multi_files,FILES)
-
-for file in files:
-   print(f'Arquivo {file} baixado!!')
-
-unzipall_files = partial(unzip_file,path = INGESTION, output = INGESTION)
-with concurrent.futures.ProcessPoolExecutor() as executor:
-    files = executor.map(unzipall_files,FILES)
-
 #ETAPA 3
-for file in os.listdir(INGESTION):
-  if file.endswith('.zip'):
-      os.remove(os.path.join(INGESTION, file))
-for file in os.listdir(INGESTION):
-   new_file = file.split('.')[-1].replace('CSV','.csv')
-   os.rename(os.path.join(INGESTION, file), os.path.join(INGESTION, new_file))
+def remove_all_files(path: str, extension: str):
+    for file in os.listdir(path):
+        if file.endswith(extension):
+            os.remove(os.path.join(path, file))
 
-#ETAPA 4
-for file in os.listdir(INGESTION):
-   print(os.path.join(INGESTION, file))
-   print(os.path.join(BRONZE,file))
-   options = pv.ParseOptions(delimiter=';')
-   table = pv.read_csv(os.path.join(INGESTION, file),parse_options = options)
-   pq.write_table(table, os.path.join(BRONZE,file.replace('csv', 'parquet')))
+@logs_decorator.csv_to_parquet
+def csv_to_parquet(path: str, output: str):
+    options = pv.ParseOptions(delimiter=';')
+    for file in os.listdir(path):
+        if file.endswith('CSV'):
+            new_file = file.split('.')[-1].replace('CSV','.csv')
+            os.rename(os.path.join(path, file), os.path.join(path, new_file))
+            table = pv.read_csv(os.path.join(path, new_file),parse_options = options)
+            pq.write_table(table, os.path.join(output,new_file.replace('csv', 'parquet')))
+    return os.listdir(output)
+
---
+++ src/logs_decorator.py
@@ -0,0 +1,35 @@
+from functools import wraps
+import time
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    filename='../LOG/logs.txt',
+    filemode='a',
+    encoding='utf-8',
+    format='%(levelname)s:%(asctime)s:%(message)s'
+    )
+
+def tempo_execucao(func):
+    @wraps(func)
+    def wrap_tempo_execicao(*args, **kwargs):
+        inicio = time.time()
+        resultado = func(*args, **kwargs)
+        fim = time.time()
+        if resultado:
+            logging.info(f'O arquivo {args[0]} demorou {fim-inicio} segundos para ser baixado.')
+        else:
+            logging.info(f'O arquivo {args[0]} não concluiu o download')
+        return resultado 
+    return wrap_tempo_execicao
+
+def csv_to_parquet(func):
+    @wraps(func)
+    def wrap_csv_to_parquet(*args, **kwargs):
+        resultado = func(*args, **kwargs)
+        for file in resultado:
+            logging.info(f'O arquivo {file} foi criado na camada Bronze.')
+        return resultado 
+    return wrap_csv_to_parquet
+
+
